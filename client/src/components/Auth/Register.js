cf9db3996d9e0f940dc32cedc1b8aa543d94e966
CurbYourStrangeness <devbuild2018@gmail.com>
2019-04-09 12:04:23 -0500

    began adding inputs on the 'Register' page (textfields and a button)

--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import Paper from '@material-ui/core/Paper'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
-
-const Register = () => (
-    <div>
-      This is the Registration Page
-    </div>
-)
+class Register extends Component {
+    render() {
+    return (
+      <Paper>
+       <form>
+         <TextField />
+         <TextField />
+         <Button />
+       </form>
+      </Paper>
+    )
+  }
+}
 
 export default Register
--- client/src/components/Layout/Main.js
+++ client/src/components/Layout/Main.js
@@ -5,8 +5,8 @@ import Header from './Header'
 const Main = ({ children }) => (
    <div>
      <Header />
-     <Grid container>
-         <Grid item>
+     <Grid container justify="center">
+         <Grid item style={{ marginTop: 30 }}>
              {children}
           </Grid>
      </Grid>
