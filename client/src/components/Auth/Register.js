e4d321cc82ff967e38b76b9eab4d78c1fd18ac2f
CurbYourStrangeness <devbuild2018@gmail.com>
2019-04-10 16:32:07 -0500

    added an event handler for changing inputs and a submission handler in src/components/Auth/Register.js

--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -26,36 +26,65 @@ class Register extends Component {
         password: '',
         password2: ''
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   } 
+
+  handleChange (e) {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+ 
+  handleSubmit (e) {
+    e.preventDefault()
+    const userData = {
+      email: this.state.email,
+      login: this.state.login,
+      password: this.state.password,
+      password2: this.state.password2
+    }
+      console.log(userData)
+  }
 
    render() {
     const { classes } = this.props;
 
     return (
       <Paper style={{ padding: 15 }}>
-       <form>
+       <form onSubmit={this.handleSubmit}>
          <TextField 
              type="email"
              label="Email"
+             name="email"
+             value={this.state.email}
+             onChange={this.handleChange}
              className={classes.textField}
           />
          <TextField 
              label="Login"
              type="text"
+             name="login"
+             value={this.state.login}
+             onChange={this.handleChange}
              className={classes.textField}
           />
           <TextField
              label="Password"
              type="password"
+             name="password"
+             value={this.state.password}
+             onChange={this.handleChange}
              className={classes.textField}
           />
           <TextField
              label="Repeat Password"
              type="password"
+             name="password2"
+             value={this.state.password2}
+             onChange={this.handleChange}
              className={classes.textField}
           />
          <div className={classes.btnBlock}>
-            <Button variant="outlined">
+            <Button variant="outlined" type="submit">
               Submit
             </Button>
          </div>
