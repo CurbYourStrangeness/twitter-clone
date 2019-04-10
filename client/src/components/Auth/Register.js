c5b992874df5f062eccbe28301507c9f4c0522e5
CurbYourStrangeness <devbuild2018@gmail.com>
2019-04-10 11:33:01 -0500

    organized the registration page

--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react'
 import Paper from '@material-ui/core/Paper'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = {
+   textField: {
+   width: '100%'
+ }
+}
+
 
 class Register extends Component {
    constructor (props) {
@@ -16,30 +24,36 @@ class Register extends Component {
 
 
    render() {
+    const { classes } = this.props;
+
     return (
       <Paper>
        <form>
          <TextField 
              type="email"
              label="Email"
+             className={classes.textField}
           />
          <TextField 
              label="Login"
              type="text"
+             className={classes.textField}
           />
           <TextField
-             label="Login"
-             type="text"
+             label="Password"
+             type="password"
+             className={classes.textField}
           />
-  <TextField
-             label="Login"
-             type="text"
+          <TextField
+             label="Repeat Password"
+             type="password"
+             className={classes.textField}
           />
-         <Button />
+         <Button variant="outlined"/>
        </form>
       </Paper>
     )
   }
 }
 
-export default Register
+export default withStyles(styles)(Register)
