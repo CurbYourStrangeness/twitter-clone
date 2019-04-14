b4026e599a92e9fe69f8c872019d1099888fc5a7
CurbYourStrangeness <devbuild2018@gmail.com>
2019-04-13 22:14:47 -0500

    installed mongoose, body-parser, and cors; began laying groundwork for db connection in server/index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 
+//setup environment
 dotenv.config()
+
+//mongo db connect
+mongoose.connect(process.env.MONGODB_URL)
+
 const app = express()
 
-
+//run app
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
