067343bbe51448ed76e4b1a766db6ea85b0fe150
CurbYourStrangeness <devbuild2018@gmail.com>
2019-04-17 18:36:21 -0500

    began handling routes and display with app.use

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv').config({ path: '.env' })
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const users = require('./routes/users')
 
 //setup environment
 dotenv.config()
@@ -10,6 +12,11 @@ mongoose.connect(process.env.MONGODB_URL, { useNewUrlparser: true })
 
 const app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+app.use('/api/users', users)
 //run app
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
