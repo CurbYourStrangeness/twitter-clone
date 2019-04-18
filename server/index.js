957ffeccffed4d40d60867e9a7853cf97b86be2f
CurbYourStrangeness <devbuild2018@gmail.com>
2019-04-18 14:46:19 -0500

    Revert "began handling routes and display with app.use"
    This reverts commit 067343bbe51448ed76e4b1a766db6ea85b0fe150.
    
    changed the package.json to try and remove nodemon

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv').config({ path: '.env' })
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const users = require('./routes/users')
 
 //setup environment
 dotenv.config()
@@ -12,11 +10,6 @@ mongoose.connect(process.env.MONGODB_URL, { useNewUrlparser: true })
 
 const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-
-
-app.use('/api/users', users)
 //run app
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
