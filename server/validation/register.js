80667fcee920bfd63569d4963e80abb14c4daefd
CurbYourStrangeness <devbuild2018@gmail.com>
2019-04-22 21:30:38 -0500

    finished valdation/register.js

--- server/validation/register.js
+++ server/validation/register.js
@@ -20,18 +20,23 @@ module.exports = function (data){
     } 
     
     if (Validator.isEmpty(data.password)) {
-        errors.password = 'Email field is required'
+        errors.password = 'Password field is required'
     } 
  
-    if (Validator.isEmpty(data.password)){
-        errors.password = ''Password Field is required'
+    if (!Validator.isLength(data.password, { min: 6, max: 30 })){
+        errors.password = 'Password must be ebtween 6 and 30 characterds'
    }
 
-   if (Validator.isLength(data.password, { min: 6, max: 30 })){
-       errors.password = 'Password must between 6 and 30 characters'
+   if (Validator.isEmpty(data.password2)){
+       errors.password = 'Confirm password is required'
    }
 
-
-  
+   if (!Validator.equals(data.password, data.password2)){
+      errors.password2 = 'Passwords must match'
+   }
+   
+   return {
+      errors,
+      isValid: Object.keys(errors).length === 0
 }
    
