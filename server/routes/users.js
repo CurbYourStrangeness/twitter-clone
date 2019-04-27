ebb10e2f4805e60e93a75164e39f8a69d6e072a2
CurbYourStrangeness <devbuild2018@gmail.com>
2019-04-27 12:10:27 -0500

    began handling registration using models and express

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
+const User = rquire('../models/User')
+const validateRegisterInput = require('../validation/register')
 
-router.route('/').get((req, res) => {
-            res.send('Hello')
+router.route('/register').get((req, res) => {
+            const { isValid, errors }
       })
 
 module.exports = router
