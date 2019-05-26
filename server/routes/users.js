1d840fa57be23c308201e75a8eabc2a76a2f8147
CurbYourStrangeness <devbuild2018@gmail.com>
2019-05-25 23:33:42 -0500

    added bcrypt functionality, will go over with vi to check close brackets

--- server/routes/users.js
+++ server/routes/users.js
@@ -17,6 +17,16 @@ router.route('/register').post((req, res) => {
                errors.email='Email was used!'
                return res.status(404).json(errors)
               }
+           
+           bcrypt.genSalt(10, function (err, salt) {
+             bcrypt.hash(req.body.password, salt, function (err, hash) {
+                const newUser = new User({ 
+                  email: req.body.email,
+                  login: req.body.login,
+                  password: hash
+               }) 
+             })
+           }) 
            const newUser = new User({
                 email: req.body.email,
                 login: req.body.login
